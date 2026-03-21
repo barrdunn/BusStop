9cd647f49c923d259c7271464fd8f2fd16fa122b
Barry Dunn <barrdunn@me.com>
2026-03-20 21:34:26 -0600

    Initial Commit

---
+++ BusStop/BusStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusStopApp.swift
+//  BusStop
+//
+//  Created by Barry Dunn on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct BusStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusStopTests/BusStopTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusStopTests.swift
+//  BusStopTests
+//
+//  Created by Barry Dunn on 3/20/26.
+//
+
+import Testing
+@testable import BusStop
+
+struct BusStopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
